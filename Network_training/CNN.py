17360d9a744fdf14f5f8d9384f126e020f956cc3
Symphony0998 <568603742@qq.com>
2017-04-02 21:34:25 +0800

    CNN
    这参数完全没法用

--- Network_training/CNN.py
+++ Network_training/CNN.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
+import Get_Database
+
+trainset_path = "D:\Code\Python\Valified_Code_Classify\Dataset\\train_set"
+testset_path = "D:\Code\Python\Valified_Code_Classify\Dataset\\test_set"
+data = Get_Database.Data(trainset_path, testset_path)
 
 sess = tf.InteractiveSession()
 
@@ -16,12 +21,12 @@ def bias_variable(shape):
 
 # convolution
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
 
 
 # pooling
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 4, 4, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 # Create the model
@@ -31,18 +36,18 @@ y_ = tf.placeholder("float", [None, 26])
 # first convolutinal layer
 w_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
-x_image = tf.reshape(x, [-1, 28, 28, 1])
+x_image = tf.reshape(x, [-1, 40, 40, 1])
 h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 # second convolutional layer
-w_conv2 = weight_variable([5, 5, 32, 64])
+w_conv2 = weight_variable([3, 3, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 # densely connected layer
-w_fc1 = weight_variable([10 * 10 * 64, 1024])
+w_fc1 = weight_variable([ 8 * 8 * 64, 1024])
 b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 10 * 10 * 64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
 # dropout
 keep_prob = tf.placeholder("float")
@@ -58,11 +63,13 @@ correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess.run(tf.global_variables_initializer())
 for i in range(20000):
-    batch = mnist.train.next_batch(100)
+    batch = data.next_batch(50)
     if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
         print(
             "step %d, train accuracy %g" % (i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    print(
+        "step %d, train accuracy %g" % (i, train_accuracy))
 print(
-    "test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    "test accuracy %g" % accuracy.eval(feed_dict={x: data.test_images, y_: data.test_labels, keep_prob: 1.0}))
