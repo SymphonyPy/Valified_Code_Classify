a0c1922971d1a568efdd16c88e4dcc6b25f5f2e5
Symphony0998 <568603742@qq.com>
2017-04-02 21:35:44 +0800

    数据集
    类似于MNIST的格式。
    Data.next_batch()有点问题，以后再改。

--- Network_training/Get_Database.py
+++ Network_training/Get_Database.py
@@ -1,13 +1,15 @@
 from PIL import Image
 import numpy as np
 import re
+import os
+import random
 
 
 def PIC(file):
     im = Image.open(file)
     im = im.convert("L")
     data = im.getdata()
-    return np.matrix(data).reshape(1, 1600)[0]
+    return np.array(data).reshape(1, 1600)[0]
 
 
 def Anwser(file):
@@ -16,3 +18,39 @@ def Anwser(file):
     matrix = np.zeros((1, 26))
     matrix[0, ord(A) - 97] = 1
     return matrix[0]
+
+
+def base(address):
+    list = []
+    for i in os.listdir(address):
+        path = address + "\\" + i
+        list.append((PIC(path), Anwser(path)))
+    return list
+
+
+class Data(object):
+    def __init__(self, trainset_path, testset_path):
+        self.train = base(trainset_path)
+        self.test = base(testset_path)
+        self.test_images = [i[0] for i in self.test]
+        self.test_labels = [i[1] for i in self.test]
+        self.order = 0
+        self.amount = len(self.train)
+
+    def next_batch(self, num):
+        mat_pic = np.zeros((num, 1600))
+        mat_ans = np.zeros((num, 26))
+        for i in range(0, num):
+            mat_pic[i] = self.train[self.order][0]
+            mat_ans[i] = self.train[self.order][1]
+            self.order = (self.order + 1) % self.amount
+        return mat_pic, mat_ans
+
+
+
+# trainset_path = "D:\Code\Python\Valified_Code_Classify\Dataset\\train_set"
+# testset_path = "D:\Code\Python\Valified_Code_Classify\Dataset\\test_set"
+# a = Data(trainset_path, testset_path)
+# print(a.test_images)
+# print(a.test_labels)
+# print(a.next_batch(2))
