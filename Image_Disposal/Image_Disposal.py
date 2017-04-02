227b983158d37507425f6ef1f29b589fb8b0a286
Symphony0998 <568603742@qq.com>
2017-04-02 21:31:37 +0800

    Revert "图片二值化"
    This reverts commit 7d93539335e94d9ad4d88b681c705023a301977f.

--- Image_Disposal/Image_Disposal.py
+++ Image_Disposal/Image_Disposal.py
@@ -5,10 +5,7 @@ import OSTU
 read_path = "D:\Code\Python\Valified_Code_Classify\\raw"
 save_path = "D:\Code\Python\Valified_Code_Classify\Dataset"
 list = os.listdir(read_path)
-num = 0
 for i in list:
-    num += 1
-    print("picture" + str(num))
     its_path = read_path + "\\" + i
     pic = OSTU.OSTU(its_path)
     Crop.Crop(pic, save_path, i)
