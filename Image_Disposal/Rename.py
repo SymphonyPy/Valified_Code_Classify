593ed0ba235806e8790bfd30031aa4bd3551bc38
Symphony0998 <568603742@qq.com>
2017-04-02 21:33:32 +0800

    数据命名格式化
    小工具，多用几次才能整理好

---
+++ Image_Disposal/Rename.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+
+def Anwser(file):
+    pattern = re.compile("\d+_(.*?).jpg")
+    A = re.findall(pattern, file)[0]
+    return A
+
+
+file = "D:\Code\Python\Valified_Code_Classify\Dataset\\train_set"
+num = 0
+list = list(os.listdir(file))
+list.sort()
+for i in list:
+    num += 1
+    old_path = file + "\\" + i
+    new_path = file + "\\" + str(num) + "_" + Anwser(file + "\\" + i) + ".jpg"
+    try:
+        os.renames(old_path, new_path)
+    except:
+        pass
+
