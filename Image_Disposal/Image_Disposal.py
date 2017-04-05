0dab47fa4acaaaa1ff0d55fab48a02cd807d2c16
Symphony0998 <568603742@qq.com>
2017-04-05 22:18:29 +0800

    将验证码而二值化并分割

--- Image_Disposal/Image_Disposal.py
+++ Image_Disposal/Image_Disposal.py
@@ -3,12 +3,12 @@ import Crop
 import OSTU
 
 read_path = "D:\Code\Python\Valified_Code_Classify\\raw"
-save_path = "D:\Code\Python\Valified_Code_Classify\Dataset"
+save_path = "D:\Code\Python\Valified_Code_Classify\\temp"
 list = os.listdir(read_path)
 num = 0
 for i in list:
     num += 1
-    print("picture" + str(num))
+    print("picture " + str(num))
     its_path = read_path + "\\" + i
     pic = OSTU.OSTU(its_path)
     Crop.Crop(pic, save_path, i)
