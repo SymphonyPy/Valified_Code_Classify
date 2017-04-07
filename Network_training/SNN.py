f0762ab70d216229f24cd51be7fd65e3602e32db
Symphony0998 <568603742@qq.com>
2017-04-07 11:35:43 +0800

    全连接网络
    shape : [1600,26]
    batch size = 100
    learning rate = 0.0001
    accuracy = 96%

--- Network_training/SNN.py
+++ Network_training/SNN.py
@@ -3,6 +3,7 @@ import Get_Database
 
 trainset_path = "D:\Code\Python\Valified_Code_Classify\Dataset\\train_set"
 testset_path = "D:\Code\Python\Valified_Code_Classify\Dataset\\test_set"
+# testset_path = "D:\Code\Python\Valified_Code_Classify\Dataset\\train_set"
 data = Get_Database.Data(trainset_path, testset_path)
 
 x = tf.placeholder(tf.float32, [None, 1600])
@@ -11,14 +12,17 @@ b = tf.Variable(tf.zeros([26]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder("float", [None, 26])
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
-init = tf.initialize_all_variables()
+train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(cross_entropy)
 sess = tf.Session()
+init = tf.initialize_all_variables()
+# saver = tf.train.Saver()
+# saver.restore(sess, "D:\Code\Python\model.ckpt")
 sess.run(init)
 for i in range(10000):
-    batch_xs, batch_ys = data.next_batch(50)
+    batch_xs, batch_ys = data.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
     if i % 100 == 0:
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-        print(sess.run(accuracy, feed_dict={x: data.test_images, y_: data.test_labels}))
+        print(sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys}))
+print(sess.run(accuracy, feed_dict={x: data.test_images, y_: data.test_labels}))
